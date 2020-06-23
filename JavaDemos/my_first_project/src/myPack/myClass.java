c07b38bc627230d5ebdd4c64ea5c071094453c3e
kunalacharya58 <kunalacharya58@gmail.com>
2020-06-23 18:21:55 +0530

    this is what we did today

--- JavaDemos/my_first_project/src/myPack/myClass.java
+++ JavaDemos/my_first_project/src/myPack/myClass.java
@@ -1,8 +1,19 @@
 package myPack;
 
 public class myClass {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("My name is Kunal");
+		Student s = new Student();
+		
+		s.setId(01);
+		s.setName("Kunal");
+		s.setAge(22);
+		s.setScore(100);
+		
+		System.out.println("Roll Number: "+s.getId());
+		System.out.println("Name: "+s.getName());
+		System.out.println("Age: "+s.getAge());
+		System.out.println("Score: "+s.getScore());
+				
 	}
 }
